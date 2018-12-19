20398bc4eb2c8f1016aadc726120cef011f44d43
Yu Ban Mena <yubanmenazabala@outlook.com>
2018-12-19 16:49:45 -0600

    Logica Y Restricciones Permitidas

--- src/App.js
+++ src/App.js
@@ -4,25 +4,32 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const siglo=21;
+    const persona = {
+      nombre:'Juan',
+      edad: 34
+    }
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            YubanJenrry <code> Hola Mundo</code> prros.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Título Nivel 1</h1> 
+        <hr/>
+      <p>Estamos en el siglo {siglo}</p>
+      <h3>Acceso a un objeto</h3>
+      <p>{persona.nombre} tiene {persona.edad} años</p>
+      <p>Un valor aleatorio llamado a un método</p>
+      {this.retornarAleatorio()}
+      <h3>Calculo inmediato de Expresiones</h3>
+      3+3={3+3}
       </div>
     );
   }
+
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 10);
+  }
+
 }
 
 export default App;
